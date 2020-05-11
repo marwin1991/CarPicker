6b0f35b5d700cccaef0ea99652a0868de4dfb45e
Nabywani <jacek.nabywaniec@gmail.com>
2020-05-11 17:53:09 +0200

    small changes in recommendations using historical data

--- python/historical_data.py
+++ python/historical_data.py
@@ -1,18 +1,32 @@
-historical_data = [{"engine" : 50, "car_body":80, "costs":48, "car_details" : 72, "equipment":80, "driving_features": 68, "chosen_models" :{"VW Passat" : 4, "Skoda Superb":3, "VW Tiguan":3}}]
+historical_data = [
+    {"engine": 50, "car_body": 80, "costs": 48, "car_details": 72, "equipment": 80, "driving_features": 68,
+     "chosen_models": {"VW Passat": 4, "Skoda Superb": 3, "VW Tiguan": 3}}]
+
+STRONG_SIMILARITY_DIFFERENCE = 5
+SMALL_SIMILARITY_DIFFERENCE = 10
 
 
-def are_similar_values(value1, value2):
-    return abs(value1 - value2) <= 5
+def are_strong_similar_values(value1, value2):
+    return abs(value1 - value2) <= STRONG_SIMILARITY_DIFFERENCE
+
+
+def are_small_similar_values(value1, value2):
+    return abs(value1 - value2) <= SMALL_SIMILARITY_DIFFERENCE
 
 
 def get_all_similar_predictions(car_elements_list):
     result = {}
     car_elements_as_string = ["engine", "car_body", "costs", "car_details", "equipment", "driving_features"]
     for historical_row in historical_data:
-        similarity = 0
+        strong_similarity = 0
+        small_similarity = 0
         for index, element in enumerate(car_elements_list):
-            if are_similar_values(element, historical_row.get(car_elements_as_string[index])):
-                similarity += 1
-        if similarity > 3:
+            if are_strong_similar_values(element, historical_row.get(car_elements_as_string[index])):
+                strong_similarity += 1
+            if are_strong_similar_values(element, historical_row.get(car_elements_as_string[index])):
+                small_similarity += 1
+        if strong_similarity > 3:
+            result.update(historical_row.get("chosen_models"))
+        elif small_similarity > 5:
             result.update(historical_row.get("chosen_models"))
     return result
--- python/main.py
+++ python/main.py
@@ -1,5 +1,6 @@
 from sklearn import preprocessing
 from sklearn.ensemble import VotingClassifier
+from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
@@ -11,15 +12,16 @@ from python.historical_data import get_all_similar_predictions
 from sklearn.linear_model import LogisticRegression
 
 MAIN_CAR_FEATURES_AMMOUNT = 6
+TRAINING_SET_SIZE = 100
 
 
 def generate_training_datasets():
-    engine = generate_engine(100, 5)
-    car_body = generate_car_body(100, 4)
-    costs = generate_costs(100, 3)
-    car_details = generate_car_details(100, 3)
-    equipment = generate_equipment(100, 3)
-    driving_features = generate_driving_features(100, 4)
+    engine = generate_engine(TRAINING_SET_SIZE, 5)
+    car_body = generate_car_body(TRAINING_SET_SIZE, 4)
+    costs = generate_costs(TRAINING_SET_SIZE, 3)
+    car_details = generate_car_details(TRAINING_SET_SIZE, 3)
+    equipment = generate_equipment(TRAINING_SET_SIZE, 3)
+    driving_features = generate_driving_features(TRAINING_SET_SIZE, 4)
     cars = get_cars()
     return engine, car_body, costs, car_details, equipment, driving_features, cars
 
@@ -88,25 +90,26 @@ if __name__ == '__main__':
     predicted_driving_features = predict(arguments.get("driving_features"), training_data[5])
     car_elements_list = [predicted_engine, predicted_car_body, predicted_costs, predicted_car_details,
                          predicted_equipment, predicted_driving_features]
-    # print(predicted_engine)
-    # print(predicted_car_body)
-    # print(predicted_costs)
-    # print(predicted_car_details)
-    # print(predicted_equipment)
-    # print(predicted_driving_features)
 
     classifiers = [
         KNeighborsClassifier(3),
         SVC(kernel="linear", C=0.025),
         SVC(gamma=2, C=1),
         DecisionTreeClassifier(max_depth=5),
+        GaussianNB()
     ]
 
     observation = [50, 80, 48, 72, 70, 45]
+    #observation = car_elements_list
 
     predicted_cars_dicts = []
     for classifier in classifiers:
         predicted_cars_dicts.append(get_cars_predicted_by_classifier(classifier, observation))
 
-    print(sorted(merge_dicts(predicted_cars_dicts).items(), key= lambda item : item[1], reverse=True))
-    print(get_all_similar_predictions(observation))
+    predicted_cars = dict(merge_dicts(predicted_cars_dicts))
+    recommended_cars_by_historical_data = dict(get_all_similar_predictions(observation))
+    print(predicted_cars)
+    print(recommended_cars_by_historical_data)
+    predicted_cars.update(recommended_cars_by_historical_data)
+    print(dict(sorted(predicted_cars.items(), key=lambda item : item[1], reverse=True)))
+
